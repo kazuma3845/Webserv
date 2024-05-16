f1e1eb9a001dfa5aefc2cc16dbfbde136c38090c
tomuller <tomuller@student.42lausanne.ch>
2024-05-16 15:14:33 +0200

    .

---
+++ srcs/main.cpp
@@ -0,0 +1,14 @@
+#include "../include/webserv.hpp"
+#include <poll.h>
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	while (1)
+	{
+		// poll();
+		break ;
+	}
+	std::cout << "TEST" << std::endl;
+	return 0;
+}
