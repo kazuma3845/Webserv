98d753a6312c2940977ffc17fd2699f5aaebac14
tomuller <tomuller@student.42lausanne.ch>
2024-05-27 11:31:05 +0200

    parsing

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -15,13 +15,17 @@
 class Web
 {
 	private:
-		std::map<std::string, std::string> _info;
+		std::map<std::string, std::map<std::string, std::string> > _info;
 
 	public:
 		Web(){};
 		~Web(){};
 
+		//main fonction
 		void check(char *argv);
 		void run() const;
 		void clean() const;
+
+		//additional fonction
+		void add_map(std::string line, std::string first_key);
 };
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
 	Web web;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
 		std::cerr << "Error, Wrong number of arguments" << std::endl;
 		return 1;
@@ -22,27 +22,59 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+void Web::add_map(std::string line, std::string first_key)
+{
+	std::string key;
+	std::string value;
+	std::string rest;
+	std::stringstream ss(line);
+
+	ss >> key >> value;
+	if (value.empty())
+		return ;
+	if (value.back() == '}')
+		value.pop_back();
+	if (value.back() == ';')
+		value.pop_back();
+	else
+	{
+		while (value.back() != ';')
+		{
+			ss >> rest;
+			value = value + " " + rest;
+		}
+		value.pop_back();
+	}
+	this->_info[first_key][key] = value;
+	// std::cout << "Key: " << key << " | Value: " << this->_info[first_key][key] << std::endl;
+}
+
 void Web::check(char *argv)
 {
 	std::string line;
-	std::string key;
-	std::string value;
 	std::ifstream file;
+	std::string key;
 
 	file.open(argv);
 	if (!file)
 		throw std::bad_exception();
 	getline(file, line);
-	
-	std::cout << line << std::endl;
 	while (getline(file, line) && !line.empty())
-	{
-    	std::stringstream ss(line);
-    	ss >> key >> value;
-		value.pop_back();
-		this->_info[key] = value;
-    	// std::cout << this->_info[key] << std::endl;
-	}
+		add_map(line, "serveur");
+	getline(file, line);
+	std::stringstream ss(line);
+	ss >> key;
+	ss >> key;
+	// std::cout << std::endl << key << std::endl;
+	while (getline(file, line) && !line.empty())
+		add_map(line, key);
+	getline(file, line);
+	std::stringstream ssi(line);
+	ssi >> key;
+	ssi >> key;
+	// std::cout << std::endl << key << std::endl;
+	while (getline(file, line) && !line.empty())
+		add_map(line, key);
 }
 
 void Web::run() const
