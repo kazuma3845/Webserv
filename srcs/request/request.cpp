8ee114a9946b1a7c670946a9de402e3d1e3de4fd
Martin Masoni <martinmasoni@macbook-pro-de-martin.home>
2024-06-18 15:18:13 +0200

    tets

--- srcs/request/request.cpp
+++ srcs/request/request.cpp
@@ -294,8 +294,8 @@ void Request::parseUri()
 		_file_path = uri;
 	_full_path = temp_root + _curr_loc.getName() + "/" + _file_path;
 	replaceDoubleSlashes(_full_path);
-	if (isDirectory(_full_path))
-		_file_path.clear();
+	if (!_file_path.empty() && isDirectory(_full_path))
+		throw fileNotFound(404);
 	_full_path = temp_root + _curr_loc.getName() + "/" + _file_path;
 	replaceDoubleSlashes(_full_path);
 	redirectInURI();
